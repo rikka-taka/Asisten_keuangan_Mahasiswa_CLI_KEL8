15836bf8f67e68557ffe7eb2f81e8c1e1d0cf41c
rikka-taka <muamadasif05@gmail.com>
2026-03-31 10:08:36 +0700

    penambahan fitur dan menu program

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// Deklarasi fungsi
+void kalkulatorDiskon();
+void splitBill();
+void bungaTabungan();
+void sisaUangJajan();
+
+int main() {
+    int pilihan;
+
+    do {
+        printf("\n=== MENU UTAMA ===\n");
+        printf("1. Kalkulator Diskon Belanja\n");
+        printf("2. Kalkulator Split Bill\n");
+        printf("3. Kalkulator Bunga Tabungan Sederhana\n");
+        printf("4. Kalkulator Sisa Uang Jajan\n");
+        printf("0. Keluar\n");
+        printf("Pilih menu: ");
+        scanf("%d", &pilihan);
+
+        switch(pilihan) {
+            case 1:
+                kalkulatorDiskon();
+                break;
+            case 2:
+                splitBill();
+                break;
+            case 3:
+                bungaTabungan();
+                break;
+            case 4:
+                sisaUangJajan();
+                break;
+            case 0:
+                printf("Terima kasih!\n");
+                break;
+            default:
+                printf("Pilihan tidak valid!\n");
+        }
+
+    } while(pilihan != 0);
+
+    return 0;
+}
