7bde1689f01626380037a1eb4438aaf9742430fa
Zaki_Azam_Adzikra <zakiazamadzikra@gmail.com>
2026-04-02 14:00:07 +0700

    penambahan fungsi sisa uang jajan calculator

--- main.c
+++ main.c
@@ -56,6 +56,47 @@ void bungaTabungan(){
 
 }
 
-void sisaUangJajan(){
+void sisaUangJajan() {
+    float uangAwal, totalPengeluaran, pengeluaran;
+    int jumlahItem;
+    char namaItem[50];
 
+    printf("\n=== KALKULATOR SISA UANG JAJAN ===\n");
+    printf("Masukkan uang jajan awal: Rp ");
+    scanf("%f", &uangAwal);
+
+    printf("Berapa banyak pengeluaran yang ingin dicatat? ");
+    scanf("%d", &jumlahItem);
+
+    totalPengeluaran = 0;
+
+    printf("\n--- Detail Pengeluaran ---\n");
+    for (int i = 1; i <= jumlahItem; i++) {
+        printf("Nama pengeluaran ke-%d: ", i);
+        scanf(" %s", namaItem);
+        printf("Jumlah biaya untuk %s: Rp ", namaItem);
+        scanf("%f", &pengeluaran);
+
+        if (pengeluaran < 0) {
+            printf("Pengeluaran tidak boleh negatif! Dilewati.\n");
+            continue;
+        }
+
+        totalPengeluaran += pengeluaran;
+    }
+
+    float sisaUang = uangAwal - totalPengeluaran;
+
+    printf("\n--- Ringkasan ---\n");
+    printf("Uang Awal       : Rp %.2f\n", uangAwal);
+    printf("Total Pengeluaran: Rp %.2f\n", totalPengeluaran);
+    printf("Sisa Uang Jajan : Rp %.2f\n", sisaUang);
+
+    if (sisaUang < 0) {
+        printf("Peringatan: Pengeluaran melebihi uang jajan sebesar Rp %.2f!\n", -sisaUang);
+    } else if (sisaUang == 0) {
+        printf("Uang jajan habis pas!\n");
+    } else {
+        printf("Kamu masih punya sisa uang jajan. Hemat terus!\n");
+    }
 }
